3e6c144d27c0f598a79bc50a46d61f87369271f7
Юрий Атаманов <atamanov.ys@gmail.com>
2020-12-28 10:12:16 +0000

    Added phpstun

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ validate:
 	composer validate
 lint:
 	composer run-script phpcs -- --standard=PSR12 src bin
+stan: 
+	composer run-script phpstan -- --level max src bin
 brain-games:
 	./bin/brain-games
 brain-even:
--- src/Cli.php
+++ src/Cli.php
@@ -6,13 +6,14 @@ use function cli\line;
 use function cli\prompt;
 use function Php\Project\Lvl1\Engine\playGame;
 
-function run($game)
+function run(string $game): bool
 {
     $name = greet();
     playGame($name, $game);
+    return true;
 }
 
-function greet()
+function greet(): string
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
